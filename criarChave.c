acd9d1e1f4cf7d6ea02d6c7d2e2223756feb6960
Djair Barral Pires <44729495+djairtb@users.noreply.github.com>
2018-11-03 16:49:46 -0300

    adicionar criarChave

--- criarChave.c
+++ criarChave.c
@@ -2,23 +2,36 @@
 #include<stdlib.h>
 #include<locale.h>
 #include<math.h>
-#include<string.h>
+#include<time.h>
 
 int main(int argc, char* argv)
 {
 	setlocale(LC_ALL,"portuguese");
-	int  numeroPrimoUm, numeroPrimoDois, tamanhoDoConjunto, euller, chavePublica;
+	int  primo[2], tamanhoDoConjunto, euller, chavePublica,chavePrivada,cont,i;
+	int primos[46] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199};
+	srand(time(NULL));
 	
-	printf("Digite um número primo:");
-	scanf("%d", &numeroPrimoUm);
-	printf("Digite um número primo:");
-	scanf("%d", &numeroPrimoDois);
-	tamanhoDoConjunto = numeroPrimoUm * numeroPrimoDois;
-	euller = (numeroPrimoUm - 1) * (numeroPrimoDois - 1);
-	printf("Digite um número primo entre 1 e %d:", euller);
-	scanf("%d", &chavePublica);
+	cont=0;
+	while(cont<2){
+		i = (rand() % 46);
+			primo[cont]=primos[i];
+		cont++;
+	}
+	//Criação da chave publica
+	tamanhoDoConjunto = primo[1] * primo[2];
+	euller = (primo[0] - 1) * (primo[1] - 1);
 	
-	printf("\nChave publica é %d | Tamanho do Conjunto é %d.\n", chavePublica, tamanhoDoConjunto);
+	chavePublica = primo[2];
+	while(chavePublica>=euller){
+	i = (rand() % 46);
+		chavePublica=primos[i];
+	}
+	//Criação da chave privada
+	chavePrivada = 1;
+	while(((chavePublica*chavePrivada)%euller) !=1 ){
+		chavePrivada++;
+	}
+	printf("\nSua chave privada é: %d\nSua chave pública é: %d\nO tamanho do conjunto é: %d\n",chavePrivada,chavePublica,tamanhoDoConjunto);
 	system ("PAUSE");
 	return 0;
 	
